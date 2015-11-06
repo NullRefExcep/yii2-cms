8177209ca359df2dd97f9f3011fe57e3c18f3d45
Dmytro Karpovych <ZAYEC77@gmail.com>
2015-11-06 15:27:13 +0200

    Update Bootstrap.php

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,53 +1,111 @@
 <?php
+namespace nullref\admin;
 
-namespace nullref\cms;
-
+use nullref\admin\models\AdminQuery;
+use yii\base\Application;
 use yii\base\BootstrapInterface;
 use yii\base\Event;
-use yii\i18n\PhpMessageSource;
+use yii\console\Application as ConsoleApplication;
 use yii\gii\Module as Gii;
+use yii\i18n\PhpMessageSource;
 use yii\web\Application as WebApplication;
 
+/**
+ * @author    Dmytro Karpovych
+ * @copyright 2015 NRE
+ */
 class Bootstrap implements BootstrapInterface
 {
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     * @param Application $app the application currently running
+     */
     public function bootstrap($app)
     {
-        if ($app instanceof WebApplication) {
-            $prefix = $app->getModule('cms')->urlPrefix;
-            $app->urlManager->addRules([
-                $prefix . '/<route:[a-zA-Z0-9-/]+>' => '/cms/page/view'
-            ]);
-            $app->controllerMap['elfinder-backend'] = [
-                'class' => 'mihaildev\elfinder\Controller',
-                'user' => 'admin',
-                'access' => ['@'],
-                'disabledCommands' => ['netmount'],
-                'roots' => [
-                    [
-                        'path' => 'uploads',
-                        'name' => 'Uploads'
-                    ],
-                ],
-            ];
+        $module = $app->getModule('admin');
 
-            $app->i18n->translations['cms*']=[
-                'class' => PhpMessageSource::className(),
-                'basePath' => '@nullref/cms/messages',
-            ];
+        $class = 'nullref\admin\models\Admin';
+
+        $definition = $class;
+        if ($module !== null) {
+            /** @var Module $module */
+            $definition = $module->adminModel;
+            if ($module->enableRbac) {
+                $module->setComponents([
+                    'authManager' => $module->authManager,
+                    'roleContainer' => $module->roleContainer,
+                ]);
+            }
         }
+
+        $app->i18n->translations['admin*']=[
+            'class' => PhpMessageSource::className(),
+            'basePath' => '@nullref/admin/messages',
+        ];
+
+        \Yii::$container->set($class, $definition);
+
+        $className = is_array($definition) ? $definition['class'] : $definition;
+
+        Event::on(AdminQuery::className(), AdminQuery::EVENT_INIT, function (Event $e) use ($class, $className) {
+            if ($e->sender->modelClass == $class) {
+                $e->sender->modelClass = $className;
+            }
+        });
+
+
+        if ($app instanceof WebApplication) {
+            \Yii::$app->setComponents(['admin' => [
+                'class' => 'nullref\admin\components\User',
+                'identityClass' => $className,
+                'loginUrl' => ['admin/login'],
+            ]]);
+            $app->urlManager->addRules(['/admin/login' => '/admin/main/login']);
+            $app->urlManager->addRules(['/admin/logout' => '/admin/main/logout']);
+        } elseif ($app instanceof ConsoleApplication) {
+            if ($module !== null) {
+                /** @var Module $module */
+                if ($module->enableRbac) {
+                    $module->controllerMap['rbac'] = [
+                        'class' => 'nullref\admin\console\RbacController',
+                    ];
+                }
+            }
+        }
+
+        Event::on(Module::className(), Module::EVENT_BEFORE_INIT, function (Event $event) use ($app) {
+            $module = $event->sender;
+            /** @var Module $module */
+            if ($module->enableRbac) {
+                if ($app instanceof ConsoleApplication) {
+                    $module->controllerMap['rbac'] = [
+                        'class' => 'nullref\admin\console\RbacController',
+                    ];
+                }
+                $module->setComponents([
+                    'authManager' => $module->authManager,
+                    'roleContainer' => $module->roleContainer,
+                ]);
+            }
+        });
 
         if (YII_ENV_DEV) {
             Event::on(Gii::className(), Gii::EVENT_BEFORE_ACTION, function (Event $event) {
                 /** @var Gii $gii */
                 $gii = $event->sender;
-                $gii->generators['block-migration-generator'] = [
-                    'class' => 'nullref\cms\generators\migration\Generator',
+                $gii->generators['crud'] = [
+                    'class' => 'yii\gii\generators\crud\Generator',
+                    'templates' => [
+                        'admin-crud' => '@nullref/admin/generators/crud/admin',
+                    ]
                 ];
-                $gii->generators['block-generator'] = [
-                    'class' => 'nullref\cms\generators\block\Generator',
+                $gii->generators['stuff'] = [
+                    'class' => 'nullref\admin\generators\stuff\Generator',
+                    'templates' => [
+                        'default' => '@nullref/admin/generators/stuff/default',
+                    ]
                 ];
             });
         }
     }
-
 }
