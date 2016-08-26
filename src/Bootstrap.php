a69d8cf97a6b41de4a35df3f0bb66277bf4edf88
Dmytro Karpovych <ZAYEC77@gmail.com>
2016-08-26 12:35:30 +0300

    fix Bootstrap

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -31,47 +31,47 @@ class Bootstrap implements BootstrapInterface
                 $definition = $classMap[$item];
                 Yii::$container->set($cmsClass, $definition);
             }
-        }
-        if ($app instanceof WebApplication) {
-            $prefix = $app->getModule('cms')->urlPrefix;
-            $app->urlManager->addRules([Yii::createObject([
-                'class' => PageUrlRule::className(),
-                'pattern' => $prefix . '/<route:[_a-zA-Z0-9-/]+>',
-            ])]);
-            if (!isset($app->controllerMap['elfinder-backend'])) {
-                $app->controllerMap['elfinder-backend'] = [
-                    'class' => 'mihaildev\elfinder\Controller',
-                    'user' => 'admin',
-                    'access' => ['@'],
-                    'disabledCommands' => ['netmount'],
-                    'roots' => [
-                        [
-                            'path' => 'uploads',
-                            'name' => 'Uploads'
+            if ($app instanceof WebApplication) {
+                $prefix = $app->getModule('cms')->urlPrefix;
+                $app->urlManager->addRules([Yii::createObject([
+                    'class' => PageUrlRule::className(),
+                    'pattern' => $prefix . '/<route:[_a-zA-Z0-9-/]+>',
+                ])]);
+                if (!isset($app->controllerMap['elfinder-backend'])) {
+                    $app->controllerMap['elfinder-backend'] = [
+                        'class' => 'mihaildev\elfinder\Controller',
+                        'user' => 'admin',
+                        'access' => ['@'],
+                        'disabledCommands' => ['netmount'],
+                        'roots' => [
+                            [
+                                'path' => 'uploads',
+                                'name' => 'Uploads'
+                            ],
                         ],
-                    ],
+                    ];
+                }
+
+                $app->i18n->translations['cms*'] = [
+                    'class' => PhpMessageSource::className(),
+                    'basePath' => '@nullref/cms/messages',
                 ];
             }
-
-            $app->i18n->translations['cms*'] = [
-                'class' => PhpMessageSource::className(),
-                'basePath' => '@nullref/cms/messages',
-            ];
-        }
-        if (YII_ENV_DEV) {
-            Event::on(Gii::className(), Gii::EVENT_BEFORE_ACTION, function (Event $event) {
-                /** @var Gii $gii */
-                $gii = $event->sender;
-                $gii->generators['block-migration-generator'] = [
-                    'class' => 'nullref\cms\generators\migration\Generator',
-                ];
-                $gii->generators['block-generator'] = [
-                    'class' => 'nullref\cms\generators\block\Generator',
-                ];
-                $gii->generators['pages-migration-generator'] = [
-                    'class' => 'nullref\cms\generators\pages_migration\Generator',
-                ];
-            });
+            if (YII_ENV_DEV) {
+                Event::on(Gii::className(), Gii::EVENT_BEFORE_ACTION, function (Event $event) {
+                    /** @var Gii $gii */
+                    $gii = $event->sender;
+                    $gii->generators['block-migration-generator'] = [
+                        'class' => 'nullref\cms\generators\migration\Generator',
+                    ];
+                    $gii->generators['block-generator'] = [
+                        'class' => 'nullref\cms\generators\block\Generator',
+                    ];
+                    $gii->generators['pages-migration-generator'] = [
+                        'class' => 'nullref\cms\generators\pages_migration\Generator',
+                    ];
+                });
+            }
         }
     }
 }
